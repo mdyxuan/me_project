8f2cadbbbc670553bbd7f4b1fb97c8c04c1b2319
mdyxuan <emily450033@gmail.com>
2024-07-17 15:59:21 +0800

    2024-07-17

--- Map/app/src/main/java/com/example/map/MainActivity.java
+++ Map/app/src/main/java/com/example/map/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
                     mapFragment.getMapAsync(MainActivity.this);
                 }
+
             }
         });
     }
